1ca54f3c1cb9aa592ce05657161f108dd9a92582
KozlowskiSzymon <Szymonztn7?yj>
2022-10-29 12:26:19 +0200

    initial commit

---
+++ src/main/java/com/example/workloadmanager/WorkloadManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.workloadmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloadManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkloadManagerApplication.class, args);
+    }
+
+}
